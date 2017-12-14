0a0ef8b2da2ad00993142b8231f9470be334bc63
usman fazil <usmanework99@gmail.com>
2017-12-14 06:56:54 +0000

    which variables should be hardcoded here.

---
+++ send_transfer.py
@@ -0,0 +1,57 @@
+from flask import Flask
+from iota.commands.extended.send_transfer import SendTransferCommand 
+from iota.commands import FilterCommand, RequestFilter
+from iota.crypto.types import Seed
+from iota.filters import Trytes
+from iota import Address, Bundle, Iota, ProposedTransaction, \
+  TransactionTrytes, TryteString
+
+
+app = Flask(__name__)
+
+
+@app.route('/put/')
+def send_transfers():
+        
+
+        transfer1 =\
+            ProposedTransaction(
+                address =
+                Address(
+                    b'TESTVALUEFIVE9DONTUSEINPRODUCTION99999MG'
+                    b'AAAHJDZ9BBG9U9R9XEOHCBVCLCWCCCCBQCQGG9WHK'
+                ),
+
+                value = 30,
+            )
+
+        transfer2 =\
+            ProposedTransaction(
+                address =
+                Address(
+                    b'TESTVALUESIX9DONTUSEINPRODUCTION99999GGT'
+                    b'FODSHHELBDERDCDRBCINDCGQEI9NAWDJBC9TGPFME'
+                ),
+
+                value = 40,
+            )
+        transfer_list=[transfer1,transfer2]
+        
+
+        request={'changeAddress':"", #optional 
+            'depth':3,
+            'inputs':None, #because it is optiona;
+            'minWeightMagnitude':18,
+            'seed':"ZCTFPRYGEAC9MGFLYLOQVGZBHDLEULMZKIXRAZPPJCAJPANAUSL9BVVAPZSVYLLWNPEYKZQEVO9A9YYP",
+            'transfers':transfer_list
+            }
+
+
+
+        A=SendTransferCommand(request)
+
+        A._execute(request)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
