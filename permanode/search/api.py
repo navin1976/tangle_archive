4848a0785dce36ba13a9fc91ffb58c3e3a43626d
usman fazil <usmanework99@gmail.com>
2018-02-01 09:17:26 +0000

    Update api.py

--- permanode/search/api.py
+++ permanode/search/api.py
@@ -7,45 +7,50 @@ from config import Logger
 @search.route('/<search_string>', methods=['GET'])
 def fetch_associated_info(search_string):
     if not search_string or len(search_string) > 90:
+        Logger.logger.error("search string greater than 90")
         abort(400)
 
     search_inst = Search(search_string)
 
     if len(search_string) <= 27:
-        Logger.logger.debug('tag')
         payload = search_inst.get_txs_for_tag()
 
         if payload is None:
+            Logger.logger.error(search_string)
             abort(404)
         
+        Logger.logger.info('tag')
         return jsonify(payload)
 
     if len(search_string) == 90:
-        Logger.logger.debug('address with checksum')
         payload = search_inst.get_txs_for_address()
 
         if payload is None:
+            Logger.logger.error(search_string)
             abort(404)
 
+        Logger.logger.info('address with checksum')
         return jsonify(payload)
 
     if len(search_string) == 81 and search_string.endswith('999'):
-        Logger.logger.debug('Transection')
         
         payload = search_inst.get_txs()
         
         if payload is None:
+            Logger.logger.error(search_string)
             abort(404)
 
+        Logger.logger.info('Transection')
         return jsonify(payload)
 
     if len(search_string) == 81 and not search_string.endswith('999'):
-        Logger.logger.debug('Bundle hash')
         payload = search_inst.get_txs_for_bundle_hash_or_address()
 
         if payload is None:
+            Logger.logger.error(search_string)
             abort(404)
 
+        Logger.logger.info('Bundle hash')
         return jsonify(payload)
-
+    Logger.logger.error(search_string)
     abort(404)
